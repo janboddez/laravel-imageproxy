f4c4e979a8d50863e100d68e2c31616ef57b2e37
Jan Boddez <jan.boddez@gmail.com>
2021-08-09 17:34:40 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@
 return [
     'secret_key' => null,
     'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
+    'ssl_verify_peer' => false,
+    'ssl_verify_peer_name' => false,
 ];

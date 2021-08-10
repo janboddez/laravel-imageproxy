5e8e6b299b2472b6609bb67e4f28c5a7a4e14967
Jan Boddez <jan.boddez@gmail.com>
2021-08-10 13:40:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'secret_key' => null,
-    'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
-    'ssl_verify_peer' => false,
-    'ssl_verify_peer_name' => false,
+    'secret_key' => env('IMAGEPROXY_KEY', ''),
+    'user_agent' => env('IMAGEPROXY_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'),
+    'ssl_verify_peer' => env('IMAGEPROXY_SSL_VERIFY_PEER', false),
+    'ssl_verify_peer_name' => env('IMAGEPROXY_SSL_VERIFY_PEER_NAME', false),
 ];

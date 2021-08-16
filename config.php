98c98e40c95e75870ddf251acb901632b808dfae
Jan Boddez <jan.boddez@gmail.com>
2021-08-16 14:45:45 +0200

    Better defaults

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
     'secret_key' => env('IMAGEPROXY_KEY', ''),
     'user_agent' => env('IMAGEPROXY_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'),
-    'ssl_verify_peer' => env('IMAGEPROXY_SSL_VERIFY_PEER', false),
-    'ssl_verify_peer_name' => env('IMAGEPROXY_SSL_VERIFY_PEER_NAME', false),
+    'ssl_verify_peer' => env('IMAGEPROXY_SSL_VERIFY_PEER', true),
+    'ssl_verify_peer_name' => env('IMAGEPROXY_SSL_VERIFY_PEER_NAME', true),
 ];
